260e34a7a8aa3ad4157516d01feb308df7270aff
Emmett Peters <ejp8611@gmail.com>
2017-07-24 13:33:36 -0500

    finished functionality of toggleing dropdown hero area

--- JS/theWorldLottery.js
+++ JS/theWorldLottery.js
@@ -1,3 +1,23 @@
-$(window).scrollTop(function(){
-	$(".heroTextOpac").css("opacity",1);
-})
+// $(window).scroll(function() {
+
+//     if ($(this).scrollTop()>-10){
+//         $('.dropdown').slideDown();
+//      } else {
+//       $('.dropdown').slideUp();
+//      }
+//  });
+
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    // Do something
+    console.log(scroll);
+    if(scroll>0){
+    	
+    	$(".dropdown").slideUp(1000);
+    	
+    } else {
+
+    	$(".dropdown").slideDown(1000);
+    }
+});
+
