6cbc4dcdd307a4edba91881708f49816b04b09b4
Emmett Peters <ejp8611@gmail.com>
2017-07-24 16:26:02 -0500

    finished iterating through header buttong for border color

--- JS/theWorldLottery.js
+++ JS/theWorldLottery.js
@@ -1,12 +1,3 @@
-// $(window).scroll(function() {
-
-//     if ($(this).scrollTop()>-10){
-//         $('.dropdown').slideDown();
-//      } else {
-//       $('.dropdown').slideUp();
-//      }
-//  });
-
 $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
     // Do something
@@ -20,4 +11,26 @@ $(window).scroll(function (event) {
     	$(".dropdown").slideDown(1000);
     }
 });
+
+var headerButtonArray =[$("#lottoB"),$("#raffleB"),$("#theWorldLottery"),$("#contact"),$("#signIn")];
+var i=0;
+
+setInterval(function(){	
+		headerButtonArray[i%5].toggleClass('buttonColor');
+		setTimeout(function(){
+			headerButtonArray[i%5].toggleClass('buttonColor');
+		i++		
+		},1900)
+		
+	},2000);
+
+// setTimeout(function(){
+// 	setInterval(function(){	
+// 		headerButtonArray[i%5].toggleClass('buttonColor');	
+// 	},1000);
+// },900)
+
+// setInterval(function(){
+// 		headerButtonArray[i%5].toggleClass('buttonColor');
+// 	},1000);
 
