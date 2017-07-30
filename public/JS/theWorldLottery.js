27f8ecb1ce8e7671060c6ea97aaeb38e9263e5db
Emmett Peters <ejp8611@gmail.com>
2017-07-30 14:34:59 -0500

    added random quote API call (ajax)to main page

--- public/JS/theWorldLottery.js
+++ public/JS/theWorldLottery.js
@@ -1,37 +1,68 @@
-//hero area scrolling functionality (custom -EP)
-$(window).scroll(function (event) {
-    var scroll = $(window).scrollTop();
-    console.log(scroll);
-    var j=0;
-    if(scroll>0 && j==0){	
-        setTimeout(function(){
-            j=1;
-        },900);
-    	$(".dropdown").slideUp(1000);
-    	setTimeout(function(){
-            $('#header').css(
-        		"background-color","black"
-            );
-        },900)
-    } else {
-        setTimeout(function(){
-            j=0;
-        },900);
-    	$(".dropdown").slideDown(1000);
-    	$('#header').css(
-    		"background-color","rgba(0,0,0,.55)"
-    	);
-        
-    }
-});
+$( document ).ready(function() {
 
-//iterating through header buttons and showing the bottom border with style (custom -EP)
-var headerButtonArray =[$("#lottoB"),$("#raffleB"),$("#theWorldLottery"),$("#contact"),$("#signIn")];
-var i=0;
-setInterval(function(){	
-	headerButtonArray[i%5].toggleClass('buttonColor');
-	setTimeout(function(){
-		headerButtonArray[i%5].toggleClass('buttonColor');
-	i++		
-	},1800)	
-},2000);
+    //hero area scrolling functionality (custom -EP)
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+        var j=0;
+        if(scroll>0 && j==0){   
+            setTimeout(function(){
+                j=1;
+            },900);
+            $(".dropdown").slideUp(1000);
+            setTimeout(function(){
+                $('#header').css(
+                    "background-color","black"
+                );
+            },900)
+        } else {
+            setTimeout(function(){
+                j=0;
+            },900);
+            $(".dropdown").slideDown(1000);
+            $('#header').css(
+                "background-color","rgba(0,0,0,.55)"
+            );   
+        }
+    });
+
+    //iterating through header buttons and showing the bottom border with style (custom -EP)
+    var headerButtonArray =[$("#lottoB"),$("#raffleB"),$("#theWorldLottery"),$("#contact"),$("#signIn")];
+    var i=0;
+    setInterval(function(){ 
+        headerButtonArray[i%5].toggleClass('buttonColor');
+        setTimeout(function(){
+            headerButtonArray[i%5].toggleClass('buttonColor');
+        i++     
+        },1800) 
+    },2000);
+
+
+
+
+
+
+    //API
+
+    function randomQuote() {
+      $.ajax({
+          url: "https://api.forismatic.com/api/1.0/?",
+          dataType: "jsonp",
+          data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+          success: function( response ) {
+            $("#random_quote").html("<p id='random_quote' class='lead text-center'>" +
+              response.quoteText + "<br/>&dash; " + response.quoteAuthor + " &dash;</p>");
+            $("#tweet").attr("href", "https://twitter.com/home/?status=" + response.quoteText +
+              ' (' + response.quoteAuthor + ')');
+          }
+      });
+    }
+
+    $(function() {
+      randomQuote();
+    });
+
+    $("#quoteButton").click(function(){
+      randomQuote();
+    });
+
+});
