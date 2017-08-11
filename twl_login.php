cdf6006fc75318d8a3fe460835cd6e6f0fa21713
Emmett Peters <ejp8611@gmail.com>
2017-08-11 09:33:52 -0500

    succesfully created my own database and user with set user table in mysql.

--- twl_login.php
+++ twl_login.php
@@ -1,6 +1,6 @@
 <?php
 
 DEFINE('DB_HOST',"127.0.0.1");
-DEFINE('DB_NAME',"twl_users_test_db");
+DEFINE('DB_NAME',"twl_test_db");
 DEFINE('DB_USERNAME',"twl_test_user");
 DEFINE('DB_PASSWORD',"password");
