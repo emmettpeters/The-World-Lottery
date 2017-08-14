15686231409dd2d1ad108b18320923c4ab2b6c33
Emmett Peters <ejp8611@gmail.com>
2017-08-14 13:17:02 -0500

    added working footer

--- public/newLayout.php
+++ public/newLayout.php
@@ -24,6 +24,7 @@
 			<a>Lotteries</a><a>Raffles</a><a>Contact us</a><a>Suggestion Box</a><a>Currency Conversions</a>
 		</div>
 	</div>
+	<div class="footer">This footer will always be positioned at the bottom of the page, but <strong>not fixed</strong>.</div>
 	
 
 </body>
