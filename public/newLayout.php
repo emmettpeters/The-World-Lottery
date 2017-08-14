d9161e78cf590cd98bf500c4272abc2c72570c0f
Emmett Peters <ejp8611@gmail.com>
2017-08-14 13:09:10 -0500

    completed basic header layout and background positioning

---
+++ public/newLayout.php
@@ -0,0 +1,52 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="CSS/newLayout.css">
+	<title>New Layout</title>
+</head>
+<body>
+	<div class="container" id="header">
+		<div class="row" id="topRow">
+			<a class="topRowButtons">Contact Us</a>
+			<h1>THE WORLD LOTTERY</h1>
+			<a class="topRowButtons">Log In</a>
+		</div>
+			<hr>
+		<div class="row" id="links">
+			<a>Lotteries</a><a>Raffles</a><a>Contact us</a><a>Suggestion Box</a><a>Currency Conversions</a>
+		</div>
+	</div>
+	
+
+</body>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script
+  src="https://code.jquery.com/jquery-3.2.1.js"
+  integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
+  crossorigin="anonymous">
+</script>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
