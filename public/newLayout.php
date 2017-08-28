15d1a357366d33fd3d9f14e47826384bbe15d360
Emmett Peters <ejp8611@gmail.com>
2017-08-28 13:30:19 -0500

    added media query to change padding on resize

--- public/newLayout.php
+++ public/newLayout.php
@@ -13,8 +13,7 @@
 	<title>New Layout</title>
 </head>
 <body>
-	<!-- header to template later -->
-	
+		
 <?php include "newLayoutHeader.php"; ?>
 <br>
 <div class="container">
@@ -26,11 +25,14 @@
 <div class="container">
 	<div class="row">
 		<div id="partOne" class="mainParts">
-			<h4>Game Area</h4><hr>
+			<h4>Game Area</h4>
+			<hr>
+			<div></div>
 		</div>
 		<div id="partTwo" class="mainParts">
 			<div id="heightMaint">
-				<h4>Chat Area</h4><hr>
+				<h4>Chat Area</h4>
+				<hr>
 				<div id='chatContent'>
 				</div>
 			</div>
@@ -38,23 +40,23 @@
 				<input id="chatInput" name="chatInput" placeholder="Say Something">
 			</form>
 			<div id="partThree" class="mainParts">
-				<h4>Other Area</h4><hr>
+				<h4>Other Area</h4>
+				<hr>
+				<div></div>
 			</div>
 		</div>
 	</div>
 </div>
-
-
-
+<br>
 <?php include "newLayoutFooter.php"; ?>	
 
 </body>
-<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script
   src="https://code.jquery.com/jquery-3.2.1.js"
   integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
   crossorigin="anonymous">
 </script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script type="text/javascript">
 	
 	$(function(){
---
+++ public/worldcoinexchange.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>World Coin Exchange</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+   <!--  <link rel="stylesheet" type="text/css" src="/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="worldcoinexchange.css">
+</head>
+<body>
+	<div class="container">
+		<!-- header -->
+		<div class="row">
+			<div id="header" class="bb col-xs-12">header</div>
+		</div>
+		<!-- navbar -->
+		<div class="row">
+			<div class="bb col-xs-12">nav</div>
+		</div>
+		<!-- main content -->
+		<div class="row">
+			<div id="mainContent" class="cc col-sm-8">
+				<!-- main area -->
+				<div class="inner1">game area
+				</div>
+			</div>
+			<div id="chatAndInfo" class="cc col-sm-4">
+				<div class="inner2">
+					<!-- chat area -->
+					<div id="chat"></div>
+					<!-- info area -->
+					<div id="info"></div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="footer">This footer will always be positioned at the bottom of the page, but <strong>not fixed</strong>.</div>
+			</div>
+		</div>
+	</div>
+
+<!-- footer -->
+<script
+  src="https://code.jquery.com/jquery-3.2.1.js"
+  integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
+  crossorigin="anonymous">
+</script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://static.filestackapi.com/v3/filestack.js"></script>
+
+
+
+
+</body>
+</html>
