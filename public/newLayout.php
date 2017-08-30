b066114f9dfde96daa880fd687627945de1292b6
Emmett Peters <ejp8611@gmail.com>
2017-08-29 19:21:35 -0500

    added tamplating to the links area and restyled the acount area

--- public/newLayout.php
+++ public/newLayout.php
@@ -16,11 +16,7 @@
 		
 <?php include "newLayoutHeader.php"; ?>
 <br>
-<div class="container">
-	<div class="row" id="links">
-		<input name="username" placeholder="Username"><input placeholder="Password" type="password" name="password"><input placeholder="Email" type="email" name="email">
-	</div>
-</div>
+<?php include "newLayoutLinks.php"; ?>
 <br>
 <div class="container">
 	<div class="row">
---
+++ public/newLayoutLinks.php
@@ -0,0 +1,5 @@
+<div class="container">
+	<div class="row" id="links">
+		<input name="username" placeholder="Username"><input placeholder="Password" type="password" name="password"><input placeholder="Email" type="email" name="email">
+	</div>
+</div>
